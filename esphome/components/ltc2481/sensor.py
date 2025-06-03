2682388749d250c15207e297df68cae41ffaf670
Anton <poos.a@ajax.systems>
2025-06-04 00:22:39 +0300

    upd

--- esphome/components/ltc2481/sensor.py
+++ esphome/components/ltc2481/sensor.py
@@ -1,7 +1,15 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor
-from esphome.const import UNIT_VOLT, DEVICE_CLASS_VOLTAGE, STATE_CLASS_MEASUREMENT
+from esphome.const import (
+    CONF_ID,
+    CONF_NAME,
+    CONF_ADDRESS,
+    CONF_UPDATE_INTERVAL,
+    DEVICE_CLASS_VOLTAGE,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_VOLT
+)
 
 DEPENDENCIES = ["i2c"]
 
@@ -12,16 +20,17 @@ CONFIG_SCHEMA = sensor.sensor_schema(
     unit_of_measurement=UNIT_VOLT,
     accuracy_decimals=4,
     device_class=DEVICE_CLASS_VOLTAGE,
-    state_class=STATE_CLASS_MEASUREMENT
+    state_class=STATE_CLASS_MEASUREMENT,
 ).extend(
     {
         cv.GenerateID(): cv.declare_id(LTC2481Sensor),
-        cv.Required("address"): cv.i2c_address,
-        cv.Optional("update_interval", default="60s"): cv.update_interval,
+        cv.Required(CONF_ADDRESS): cv.i2c_address,
+        cv.Optional(CONF_UPDATE_INTERVAL, default="60s"): cv.update_interval,
     }
 ).extend(i2c.i2c_device_schema(0x24))
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[cv.GenerateID()])
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
+    await sensor.register_sensor(var, config)
